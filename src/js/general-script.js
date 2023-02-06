6cf89d25fba591a441174753cee8051ed637fd7a
Edwin Nieto <egnieto94@gmail.com>
2023-02-06 00:22:11 -0500

    carousel y ajustes finales (pendiente form)

--- src/js/general-script.js
+++ src/js/general-script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
 
-	$('.owl-carousel').owlCarousel({
+	$('#carousel-index').owlCarousel({
 		loop: true,
 		margin: 10,
 		dots: true,
@@ -21,10 +21,54 @@ $(document).ready(function () {
 			}
 		}
 	});
+	$('#carousel-sedes').owlCarousel({
+		loop: true,
+		margin: 20,
+		dots: true,
+		nav: true,
+		mouseDrag: true,
+		autoplay: true,
+		autoplayHoverPause: false,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 2
+			},
+			1000: {
+				items: 5
+			}
+		}
+	});
 
 	$('.fa.fa-bars').click(function () {
 		event.preventDefault();
 		$('li.item-mm').toggleClass('active');
+	});
+	$('.link.dropdown').click(function () {
+		event.preventDefault();
+		$("a").on('click', function (event) {
+
+			// Make sure this.hash has a value before overriding default behavior
+			if (this.hash !== "") {
+				// Prevent default anchor click behavior
+				event.preventDefault();
+
+				// Store hash
+				var hash = this.hash;
+
+				// Using jQuery's animate() method to add smooth page scroll
+				// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+				$('html, body').animate({
+					scrollTop: $(hash).offset().top
+				}, 300, function () {
+
+					// Add hash (#) to URL when done scrolling (default click behavior)
+					window.location.hash = hash;
+				});
+			} // End if
+		});
 	});
 
 
